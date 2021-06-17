e23708ec5164c464ede75313362d903ea7c7b596
Abhishek Saxena <abhishek.saxena@factset.com>
2021-06-18 00:53:54 +0530

    toggle led changes pushed

--- Cylon_Prj/cylon-server/controller/robot.js
+++ Cylon_Prj/cylon-server/controller/robot.js
@@ -5,7 +5,7 @@ let robot = null;
 exports.configureRobot = async (req, res, next) => {
   try {
     console.log("Req", req.body);
-    //robot = cylon.robot(req.body).start();
+    // robot = cylon.robot(req.body).start();
     console.log("Robot configuration done..!!!");
     res.send({ statue: "Success", data: "Robot configuration done..!!!" });
   } catch {
@@ -28,6 +28,16 @@ exports.stopLED = async (req, res, next) => {
     // robot.devices.led.turnOff();
     console.log("Led Stop");
     res.send({ statue: "Success", data: "Led Stop" });
+  } catch {
+    next(e);
+  }
+};
+
+exports.toggle = async (req, res, next) => {
+  try {
+    // robot.devices.led.toggle();
+    console.log("Led Toggle");
+    res.send({ statue: "Success", data: "Led Toggle" });
   } catch {
     next(e);
   }
